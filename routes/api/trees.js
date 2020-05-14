f202e1f51b0e7df97800215c61484a578aef6d10
Mohammed Yaseen <Moha.98.1900@gmail.com>
2020-05-14 15:23:12 +0300

    debugged api routes

--- routes/api/trees.js
+++ routes/api/trees.js
@@ -1,12 +1,54 @@
+const logic = require('../../build/Release/logic.node');
+const session = require('express-session');
 const express = require('express');
 const router = express.Router();
 
+router.use(session({
+    secret : 'Key773_-+3Vvx||d121dddvsCZXW@$%^&$#fsghdsb',
+    resave : false,
+    saveUninitialized : false,
+}));
 
-// @route  GET api/items
+// @route  GET api/trees/getTree
 // @desc   get the current tree
 // @access public
 router.get('/getTree', (req, res) => {
     res.send({'tree-string': '{5}({4}{6})', 'id': 5});
 });
 
+// @route  POST api/trees/sendTree
+// @desc   send a tree
+// @access public
+router.post('/sendTree', (req, res) => {
+    // Creating a new tree object & initializing it from tree string recieved
+    req.session.tree = new logic.Tree23();
+    req.session.tree.constructFromTreeString(req.body.treeString);
+
+    // Send back the tree just recieved after adding it to json formatted object
+    treeObj = {'id': 0, 'treeString': ''};
+    treeObj.id = 0; // will be fixed later
+    treeObj.treeString = req.session.tree.toTreeString();
+    res.send(treeObj);
+});
+
+// @route  POST api/trees/addValue
+// @desc   sends one value to be added to tree
+// @access public
+router.post('/addValue', (req, res) => {
+    // Creating a new tree object & initializing it from tree string recieved
+    req.session.tree = new logic.Tree23();
+    req.session.tree.constructFromTreeString(req.body.treeString);
+    // insert the value into the tree
+    req.session.tree.insert(parseInt( req.body.value ));
+
+    // Send back the tree just recieved after adding it to json formatted object
+    treeObj = {'id': 0, 'treeString': ''};
+    treeObj.id = 0; // will be fixed later
+    treeObj.treeString = req.session.tree.toTreeString();
+    res.send(treeObj);
+});
+
+
+// Exporting logic
+module.exports = logic;
 module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,15 @@
 const express = require('express');
-const logic = require('./build/Release/logic.node');
 // const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-
+const bodyParser = require('body-parser');
 const trees = require('./routes/api/trees');
 const app = express();
 
 // body-parser middleware
-// app.use(bodyParser.json);
-
-// TEST
-var tree = new logic.Tree23();
-tree.insert(5);
-tree.insert(2);
-tree.insert(7);
-app.get('/', (req, res) => {
-    res.send(tree.toTreeString());
-})
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // redirecting api requests to items
 app.use('/api/trees', trees);
-
-
-// Exporting logic
-module.exports = logic;
 
 // Port configurations
 const port = process.env.PORT || 2000;
