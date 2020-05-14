87498084df8479944579883add96424aa8de48e3
Mohammed Yaseen <Moha.98.1900@gmail.com>
2020-05-14 23:14:09 +0300

    fixed typo in import path

--- client/src/components/canvasContainer.js
+++ client/src/components/canvasContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './styles/canvas.css'
+import '../Styles/canvas.css'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
