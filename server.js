c40b7d2d34a1451d53a64c0a976ad96b6ceece87
Mohammed Yaseen <Moha.98.1900@gmail.com>
2020-05-14 14:19:32 +0300

    testing addon

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ const app = express();
 // body-parser middleware
 // app.use(bodyParser.json);
 
+// TEST
+var tree = new logic.Tree23();
+tree.insert(5);
+tree.insert(2);
+tree.insert(7);
+app.get('/', (req, res) => {
+    res.send(tree.toTreeString());
+})
+
 // redirecting api requests to items
 app.use('/api/trees', trees);
 
