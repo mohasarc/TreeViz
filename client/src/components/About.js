7f976a1c74b1e63077d490d630eb988da2d63ee9
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2020-08-24 00:58:31 +0300

    editted about and how-to

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import Header from './header'
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import React from 'react';
+import Header from './header';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class About extends React.Component{
     constructor(props){
@@ -21,19 +23,18 @@ class About extends React.Component{
             <div>
                 <Header/>
                 <Jumbotron>
-                    <br/>
-                    <p>This website was Developped by Mohammed S. Yaseen. It is intended to be a helpful tool
+                    <Row xs={12} md={12} lg={12}>
+                        <Col md={{ span: 6, offset: 3 }} lg={{ span: 6, offset: 3 }} style={{"text-align":"left"}}>This website was Developped by Mohammed S. Yaseen. It is intended to be a helpful tool
                         to visualize trees mainly for study purposes. Currently, the app is under development
-                        and many features are yet to be implemented..
-                    </p>
-                    <p>
-                        Node js, Express js, and React js were used to develop this website, 
-                        in addition to using node addons api to compile and run c++ logic libraries, s.t. 
-                        the trees logic, in the backend.
-                    </p>
-                    <p>
-                        latest update: 15/5/2020
-                    </p>
+                        and many features are yet to be implemented.
+                        </Col>
+                    </Row>
+                    <br/>
+                    <Row>
+                        <Col md={{ span: 6, offset: 3 }} lg={{ span: 6, offset: 3 }} style={{"text-align":"left"}}>
+                        latest update: 24/08/2020
+                        </Col>
+                    </Row>
                 </Jumbotron>
                 <footer className="blockquote-footer">
                     This website was developped by <a href='https://github.com/mohasarc'>Mohammed S. Yaseen</a>
--- client/src/components/HowTo.js
+++ client/src/components/HowTo.js
@@ -1,6 +1,9 @@
-import React from 'react'
-import Header from './header'
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import React from 'react';
+import Header from './header';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 class HowTo extends React.Component{
     constructor(props){
         super(props);
@@ -20,33 +23,45 @@ class HowTo extends React.Component{
             <div>
                 <Header/>
                 <Jumbotron>
-                    <h3>
-                        How to use TreeViz
-                    </h3>
+                    <Row xs={12} md={12} lg={12}>
+                        <Col md={{ span: 6, offset: 3 }} lg={{ span: 6, offset: 3 }} style={{"text-align":"left"}}>
+                            <h3> How to use TreeViz </h3>
+                        </Col>
+                    </Row>
                     <br/>
-                    <p style={{'text-align': 'left'}}>    There are two different ways create a tree. Either start with and empty tree <br/>
-                        and add values using + button, or use tree string structure to describe a <br/>
-                        particular tree then perform operations on it. <br/>
+                    <Row xs={12} md={12} lg={12}>
+                        <Col md={{ span: 6, offset: 3 }} lg={{ span: 6, offset: 3 }} style={{"text-align":"left"}}>
+                            There are two different ways create a tree. Either start with and empty tree
+                            and add values using + button, or use tree string structure to describe a
+                            particular tree then perform operations on it. A third way also exists that is
+                            generating a random tree using random tree generator by pressing Generate button.
+                        </Col>
+                    </Row>
                     <br/>
-                    <p> What is tree string? <br/>
-                     It is a string that explains the tree.<br/>
-                     - Each number between {'{}'} belonges to the same node<br/>
-                     - Each node's children are placed between ()<br/>
-                        ex:<br/>
-                        {'     {5,7}'}    <br/>
-                        {' /     |     \\'}  <br/>
-                        {'{3}   {6}   {8}'}<br/>
-                       tree string : {'{5,7}({3}{6}{8})'}<br/>
-                    </p>
-                    <p>
-                        Currently only 2-3 tree is implemented and it can only construct a tree from a tree string
-                        and add values to empty or non-empty tree.
-                    </p>
-                    <p>
-                        Important note: the tree string should contain a valid 2-3 tree that is if a node has one item,
-                        it can have only two children and if it has two items it must have 3 children.
-                    </p>
-                    </p>
+                    <Row>
+                        <Col md={{ span: 6, offset: 3 }} lg={{ span: 6, offset: 3 }} style={{"text-align":"left"}}>
+                         <h5>  What is tree string?  </h5>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={{ span: 6, offset: 3 }} lg={{ span: 6, offset: 3 }} style={{"text-align":"left"}}>
+                        It is a string that explains the tree such that:<br/>
+                            &nbsp;&nbsp;&nbsp;&nbsp;- numbers separated by commas and between {'{}'} belonges to the same node<br/>
+                            &nbsp;&nbsp;&nbsp;&nbsp;- Each node's children are separated by commas and placed between ()<br/>
+                            example tree string : {'{5,7}({3}{6}{8,9})'}<br/>
+                            corresponding tree :
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={{ span: 6, offset: 3 }} lg={{ span: 6, offset: 3 }} style={{"text-align":"center"}}>
+                            {'     {5,7}'}    <br/>
+                            {' /     |     \\'}  <br/>
+                            {'{3}   {6}   {8,9}'}<br/>
+                        <br/>
+
+                        </Col>
+                    </Row>
+                    <br/>
                 </Jumbotron>
                 <footer className="blockquote-footer">
                 This website was developped by <a href='https://github.com/mohasarc'>Mohammed S. Yaseen</a>
